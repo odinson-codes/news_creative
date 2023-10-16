8ef6de70acb1780bc93542349e38d1884f80c02b
odinson-codes <lamichhaneumang123@gmail.com>
2023-10-16 16:54:23 +0545

    Making not found, footer  page other small changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,33 @@ export default function Home() {
   return (
     <div>
      <div>
-     <div className="border-t border-gray-300 my-4"></div>
-     <div className="border-t border-gray-300 my-4"></div>
-     <div className="border-t border-gray-300 my-4"></div>
-     <div className="border-t border-gray-300 my-4"></div>
-     <div className="border-t border-gray-300 my-4"></div>
-     <div className="border-t border-gray-300 my-4"></div>
-     <div className="border-t border-gray-300 my-4"></div>
-     <div className="border-t border-gray-300 my-4"></div>
-     <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
+      <div className="border-t border-gray-300 my-4"></div>
      </div>
     </div>
   )
